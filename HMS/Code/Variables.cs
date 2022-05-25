63ce930db1bccf8f95e8432710dbef088230f133
Amitesh Mishra <amitesh18896@gmail.com>
2022-05-25 14:12:55 +0530

    Add project files.

---
+++ HMS/Code/Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMS.Code
+{
+    public class Variables
+    {
+        public const string HotelFullName = "Amitesh";
+        public const string HotelShortName = "Amitesh";
+        public const string HotelNameAbbreviate = "Anu";
+    }
+}
